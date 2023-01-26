2c2642edd8b2f7da16dd8cab8c576da933f1ee2b
ajayjamliya <ajayjamliya76@gmail.com>
2023-01-26 11:13:49 +0530

    all are committed in single got

---
+++ Sbi-bank/Dbutils.java
@@ -0,0 +1,7 @@
+Updating for git stash 
+
+Hi this is Db utils file conating Db related informations
+
+updating it for development branch
+
+updating now
---
+++ Sbi-bank/Stringutils.java
@@ -0,0 +1,6 @@
+updating for Git stash in master branch
+
+This file is containig String utils file
+
+TESTING FOR git 2 development
+updating for command git commit -a -m "second commit for Stringutils"
